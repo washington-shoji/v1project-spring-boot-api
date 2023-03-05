84da666d91f80bb5afb263d6c612fff574226486
washington-shoji <shoji.washington@gmail.com>
2023-03-05 11:18:50 +1100

    first commit

---
+++ src/main/groovy/com/v1project/v1project/V1projectApplication.groovy
@@ -0,0 +1,13 @@
+package com.v1project.v1project
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class V1projectApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(V1projectApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/v1project/v1project/V1projectApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.v1project.v1project
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class V1projectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
