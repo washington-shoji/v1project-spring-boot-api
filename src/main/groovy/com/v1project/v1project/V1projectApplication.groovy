05c8ed89522e25f2410ba90ad4955e6ecb7d8968
washington-shoji <shoji.washington@gmail.com>
2023-04-25 12:10:42 +1000

    refactor(Core): App Config
    - Updated the app config class with cors filter

--- src/main/groovy/com/v1project/v1project/V1projectApplication.groovy
+++ src/main/groovy/com/v1project/v1project/V1projectApplication.groovy
@@ -2,12 +2,28 @@ package com.v1project.v1project
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 class V1projectApplication {
 
 	static void main(String[] args) {
 		SpringApplication.run(V1projectApplication, args)
+	}
+
+	@Bean
+	public WebMvcConfigurer corsMappingConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:4200/")
+						.allowedHeaders("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+			}
+		}
 	}
 
 }
