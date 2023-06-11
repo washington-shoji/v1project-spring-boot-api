cc468227347a89df2d3b3aaf362fc5079f24ed0e
washington-shoji <shoji.washington@gmail.com>
2023-06-11 19:34:57 +1000

    feat(CI/CD Testing): CI/CD Testing
    - CI/CD Testing v5

--- src/main/groovy/com/v1project/v1project/entities/User.java
+++ src/main/groovy/com/v1project/v1project/entities/User.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
