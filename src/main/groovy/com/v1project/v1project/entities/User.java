43750a4220cf9613386140fe1cd9e031d01d85b9
washington-shoji <shoji.washington@gmail.com>
2023-06-11 18:21:10 +1000

    feat(Entity): User Entity
    - Added User Entity

---
+++ src/main/groovy/com/v1project/v1project/entities/User.java
@@ -0,0 +1,19 @@
+package com.v1project.v1project.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
